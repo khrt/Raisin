594e271b9ed73c4aa61b7d112bc0c96b6b920b58
Artur Kh <khrt@ya.ru>
2014-10-31 15:32:59 +0200

    UPdate example relations

--- examples/entity/lib/MusicApp/Schema/Result/Artist.pm
+++ examples/entity/lib/MusicApp/Schema/Result/Artist.pm
@@ -9,6 +9,6 @@ __PACKAGE__->table('artists');
 __PACKAGE__->add_columns(qw/id name/);
 __PACKAGE__->set_primary_key('id');
 
-__PACKAGE__->has_many('albums', 'MusicApp::Schema::Result::Album');
+__PACKAGE__->has_many('albums', 'MusicApp::Schema::Result::Album', 'artist_id');
 
 1;
