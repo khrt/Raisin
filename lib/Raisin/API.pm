dab337fd94a627dffd4b8850e55f507c86f784dc
Artur Kh <khrt@ya.ru>
2014-11-01 20:35:22 +0200

    Add `present` function

--- lib/Raisin/API.pm
+++ lib/Raisin/API.pm
@@ -10,7 +10,7 @@ use Raisin;
 
 my @APP_CONF_METHODS = qw(api_default_format api_format api_version middleware mount plugin);
 my @APP_EXEC_METHODS = qw(new run);
-my @APP_METHODS = qw(req res param session);
+my @APP_METHODS = qw(req res param session present);
 my @HOOKS_METHODS = qw(before before_validation after_validation after);
 my @HTTP_METHODS = qw(del get head options patch post put);
 my @ROUTES_METHODS = qw(resource namespace route_param desc params);
@@ -212,6 +212,25 @@ sub param {
     $app->req->parameters->mixed;
 }
 sub session { $app->session(@_) }
+
+sub present {
+    my ($key, $data, %params) = @_;
+
+    my $value = do {
+        if (my $entity = $params{with}) {
+            $entity->compile($data);
+        }
+        else {
+            $data;
+        }
+    };
+
+    my $body = res->body || {};
+    my $representation = { $key => $value, %$body };
+    res->body($representation);
+
+    undef;
+}
 
 #
 # System
